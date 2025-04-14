fc10e11a5006ca92b747fee289edb94c6d554265
Arne <arne.eckert@stud.leuphana.de>
2025-04-14 10:36:44 +0200

    Initial commit with README and week1 assignment

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# TechBasics1
+# 💻 Tech-Basics-1
+
+Hiii :)  
+This is my repository for the *Tech Basics 1* seminar
+
+---
+
+## 📚 Weekly Stuff
+
+Each week's tasks are in their own folder in `assignments/`  
+
+
+---
+
+## 🐍 Python
+
+The goal is to get comfortable with the **basics of Python**, and hopefully pick up some practical skills..
+
+
+---
+
+
+
+![Python Logo](https://www.python.org/static/community_logos/python-logo.png)
---
+++ assignments/week1/assignment1.py
@@ -0,0 +1,19 @@
+# tried to make a human as first exercise with print functions :)
+# \\ means literally \, because only one \ is a special character (usecases: \n = new line ; \t = tab
+
+print("       _____ ")
+print("     /       \\")
+print("    |  O   O  |")
+print("    |    >    |")
+print("    |  \\___/  |")
+print("     \\_______/")
+print("        ||")
+print("      __||__")
+print("     |  ||  |")
+print("     |  ||  |")
+print("     |__||__|")
+print("       /\\")
+print("      /  \\")
+print("_____/____\\__________")
+
+
